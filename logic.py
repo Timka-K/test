59cd69711a5d89bfe252cd7cd0df99fa1de2b259
Timka-K <fkardanova41@gmail.com>
2025-06-07 14:11:31 +0300

    Новые функции

--- logic.py
+++ logic.py
@@ -11,12 +11,22 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.height = self.get_height()
+        self.weight = self.get_weight()
+        self.abilities = self.get_abilities()
+        
 
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
     def get_img(self):
-        pass
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/{self.pokemon_number}.png'
+        else:
+            return "Photo is not found"
     
     # Метод для получения имени покемона через API
     def get_name(self):
@@ -27,11 +37,44 @@ class Pokemon:
             return (data['forms'][0]['name'])
         else:
             return "Pikachu"
+        
+# Метод для получения высоты покемона через API
+    def get_height(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['height'])
+        else:
+            return "-"
+        
+# Метод для получения ширины покемона через API
+    def get_weight(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['weight'])
+        else:
+            return "-"
 
+# Метод для получения способностей покемона через API
+    def get_abilities(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            abilities = []
+            for ability in data.get('abilities', []):
+                ability_name = ability['ability']['name']
+                is_hidden = ability['is_hidden']
+                abilities.append(f"{ability_name}" + (" (hidden)" if is_hidden else ""))
+            return abilities
+        return []
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя твоего покеомона: {self.name}\nВысота: {self.height}\nШирина: {self.weight}\nСпособности: {self.abilities}"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ bot = telebot.TeleBot(token)
 def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
         pokemon = Pokemon(message.from_user.username)
-        bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
+        bot.send_message(message.chat.id, pokemon.info())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
